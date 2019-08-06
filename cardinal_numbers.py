01d0c48b1b89f98608588431852369b9a2017182
tnthien <thienta.028@gmail.com>
2019-08-06 15:37:10 +0700

    Fix bugs

--- cardinal_numbers.py
+++ cardinal_numbers.py
@@ -1,31 +1,33 @@
 import pygame
 
 pygame.init()
-sound_dict = dict(không='khong.ogg', một='mot1.ogg', mốt='mot2.ogg', hai='hai.ogg', ba='ba.ogg', bốn='bon.ogg',
+vn_sound_dict = dict(không='khong.ogg', một='mot1.ogg', mốt='mot2.ogg', hai='hai.ogg', ba='ba.ogg', bốn='bon.ogg',
                   năm='nam.ogg', lăm='lam.ogg', sáu='sau.ogg', bảy='bay.ogg', tám='tam.ogg', chín='chin.ogg',
                   mươi='muoi2.ogg', mười='muoi1.ogg', nghìn='nghin.ogg', triệu='trieu.ogg', trăm='tram.ogg',
                   linh='linh.ogg', lẻ='le.ogg', tỷ='ty.ogg')
-vn_digits_dict = {0: 'không', 1: 'một', -1: 'mốt', 2: 'hai', 3: 'ba', 4: 'bốn', 5: 'năm',
+vn_digits_in_units = {0: 'không', 1: 'một', -1: 'mốt', 2: 'hai', 3: 'ba', 4: 'bốn', 5: 'năm',
                   -5: 'lăm', 6: 'sáu', 7: 'bảy', 8: 'tám', 9: 'chín'}
-power_of_1k_dict = {0: '', 1: ' nghìn', 2: ' triệu', 3: ' tỷ'}
-power_of_1k_dict_eng = {0: '', 1: ' thousand', 2: ' million', 3: ' billion'}
+vn_powers_of_1k = {0: '', 1: ' nghìn', 2: ' triệu', 3: ' tỷ'}
+eng_powers_of_1k = {0: '', 1: ' thousand', 2: ' million', 3: ' billion'}
 digits_in_units = {0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four',5: 'five',
                 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
 digits_in_tens = {0: '', 1: 'ten', 2: 'twenty', 3: 'thirty', 4: 'forty',
                   5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'}
-patch_num = {'ten-one': 'eleven', 'ten-two': 'twelve', 'ten-three': 'thirteen',
+eng_nums_patch = {'ten-one': 'eleven', 'ten-two': 'twelve', 'ten-three': 'thirteen',
              'ten-four': 'fourteen', 'ten-five': 'fifteen', 'ten-six': 'sixteen',
              'ten-seven': 'seventeen', 'ten-eight': 'eighteen', 'ten-nine': 'nineteen'}
 
 
 # Check validity of input:
-def raise_exceptions(n):
+def raise_exceptions(n,activate_tts):
     if not isinstance(n, int):
         raise TypeError('Not a integer')
     if not n >= 0:
         raise ValueError('Not a positive integer')
     if n > 999999999999:
         raise OverflowError('Maximum value exceeded')
+    if activate_tts != None and not isinstance(activate_tts, bool):
+        raise TypeError('Not a boolean')
 
 
 # Split the number into lists of numbers less than 1k
@@ -45,25 +47,24 @@ def fix_abnormal_combs(u):
 
 
 # Fix other VN abnormal combinations for the leftmost digits:
-y = lambda u: u.replace('không trăm linh', '').replace('không trăm', '')
+vn_further_repl = lambda u: u.replace('không trăm linh', '').replace('không trăm', '')
 
 # South-North string converter
 southside = lambda u: u.replace('linh', 'lẻ').replace('nghìn', 'ngàn')
 
 
 def integer_to_vietnamese_numeral(n, region='north', activate_tts=False):
-    raise_exceptions(n)
+    raise_exceptions(n,activate_tts)
     if not isinstance(region, type(None)):
         if not isinstance(region, str):
             raise TypeError('Invalid region input type')
         elif not region in ('south', 'north'):
             raise ValueError('Invalid region')
-    if activate_tts != None and not isinstance(activate_tts, bool):
-        raise TypeError('Not a boolean')
+
     if n == 0:
         return "không"
 
-    def process_nums_less_than_1k(n):
+    def vn_process_nums_less_than_1k(n):
         if n == 0:
             return ''
         digit_list = []
@@ -76,16 +77,17 @@ def integer_to_vietnamese_numeral(n, region='north', activate_tts=False):
             digit_list[0] = -1
         if digit_list[0] == 5 and digit_list[1] != 0:
             digit_list[0] = -5
-        nume_list = [vn_digits_dict[i] for i in digit_list]
-        op = '{} trăm {} mươi {}'.format(nume_list[-1], nume_list[-2], nume_list[-3])
-        return fix_abnormal_combs(op)
+        nume_list = [vn_digits_in_units[i] for i in digit_list]
+        output = '{} trăm {} mươi {}'.format(nume_list[-1], nume_list[-2], nume_list[-3])
+        return fix_abnormal_combs(output)
 
     # Format each element in the lists
-    vn_numerals_list = [process_nums_less_than_1k(i) for i in nums_less_than_1k]
-    vn_numerals_list[-1] = y(vn_numerals_list[-1])
+    nums_less_than_1k = num_splitting(n)
+    vn_numerals_list = [vn_process_nums_less_than_1k(i) for i in nums_less_than_1k]
+    vn_numerals_list[-1] = vn_further_repl(vn_numerals_list[-1])
     for i in range(len(vn_numerals_list)):
         if vn_numerals_list[i]:
-            vn_numerals_list[i] += power_of_1k_dict[i]
+            vn_numerals_list[i] += vn_powers_of_1k[i]
     vn_numerals_list.reverse()
     # Join the list into a string
     raw_result = ' '.join(vn_numerals_list).split()
@@ -96,7 +98,7 @@ def integer_to_vietnamese_numeral(n, region='north', activate_tts=False):
     # Play sound (if necessary)
     if activate_tts:
         for i in raw_result:
-            sound = pygame.mixer.Sound('./vie/{}/{}'.format(region, sound_dict[i]))
+            sound = pygame.mixer.Sound('./vie/{}/{}'.format(region, vn_sound_dict[i]))
             sound.play()
             pygame.time.delay(500)
             pygame.mixer.stop()
@@ -104,43 +106,43 @@ def integer_to_vietnamese_numeral(n, region='north', activate_tts=False):
 
 
 def integer_to_english_numeral(n,activate_tts = False):
-    raise_exceptions(n)
+    raise_exceptions(n,activate_tts)
     if n == 0:
         return "Zero"
 
-    def process_num_eng(n):
-        digit_lst = []
+    def eng_process_nums_less_than_1k(n):
+        digit_list = []
         for i in range(0,3):
-            digit_lst.append(n % 10)
+            digit_list.append(n % 10)
             n //= 10
-        digit_lst.reverse()
-        if digit_lst[0] != 0:
-            digit_lst[0] = digits_in_units[digit_lst[0]] + " hundred"
+        digit_list.reverse()
+        if digit_list[0] != 0:
+            digit_list[0] = digits_in_units[digit_list[0]] + " hundred"
         else:
-            digit_lst[0] = ''
+            digit_list[0] = ''
         def repl(x,y):
             for i in x:
-                if i == digit_lst[y]:
-                    digit_lst[y] = x[i]
-            return digit_lst[y]
-        digit_lst[1] = repl(digits_in_tens, 1)
-        digit_lst[2] = repl(digits_in_units, 2)
-        if digit_lst[1] and digit_lst[2]:
-             digit_lst[2] = '-' + digit_lst[2]
-        digit_lst[1:3] = [''.join(digit_lst[1:3])]
-        for i in patch_num:
-            digit_lst[1] = digit_lst[1].replace(i, patch_num[i])
-        if not digit_lst[0] or not digit_lst[1]:
-            output = ''.join(digit_lst)
+                if i == digit_list[y]:
+                    digit_list[y] = x[i]
+            return digit_list[y]
+        digit_list[1] = repl(digits_in_tens, 1)
+        digit_list[2] = repl(digits_in_units, 2)
+        if digit_list[1] and digit_list[2]:
+             digit_list[2] = '-' + digit_list[2]
+        digit_list[1:3] = [''.join(digit_list[1:3])]
+        for i in eng_nums_patch:
+            digit_list[1] = digit_list[1].replace(i, eng_nums_patch[i])
+        if not digit_list[0] or not digit_list[1]:
+            output = ''.join(digit_list)
         else:
-            output = digit_lst[0] + ' and ' + digit_lst[1]
+            output = digit_list[0] + ' and ' + digit_list[1]
         return output.strip()
 
     nums_less_than_1k = num_splitting(n)
-    eng_numerals_list = [process_num_eng(i) for i in nums_less_than_1k]
+    eng_numerals_list = [eng_process_nums_less_than_1k(i) for i in nums_less_than_1k]
     for i in range(len(eng_numerals_list)):
         if eng_numerals_list[i]:
-            eng_numerals_list[i] += power_of_1k_dict_eng[i]
+            eng_numerals_list[i] += eng_powers_of_1k[i]
     eng_numerals_list.reverse()
     if eng_numerals_list[-1]:
         eng_numerals_list[-1] = 'and {}'.format(eng_numerals_list[-1])
@@ -151,8 +153,10 @@ def integer_to_english_numeral(n,activate_tts = False):
     if activate_tts == True:
         value_for_tts = result.replace('-',' ').split()
         for i in value_for_tts:
-            sound = pygame.mixer.Sound('./vie/{}/{}'.format(region, sound_dict[i]))
+            sound = pygame.mixer.Sound('./eng/{}.ogg'.format(i))
             sound.play()
             pygame.time.delay(500)
             pygame.mixer.stop()
     return result
+
+print(integer_to_english_numeral(201000210125))
